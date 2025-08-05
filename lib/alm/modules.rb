58fe7ec469c5c5db1e9cc3321e53e76486eae09b
mvguest <vanproller@gmail.com>
2025-08-05 04:34:17 -0300

    Refactoring code structure and add remove module

--- lib/alm/modules.rb
+++ lib/alm/modules.rb
@@ -4,40 +4,66 @@ module Modules
   class ADD
     def self.start(args)
       file_arg = args.first
-      file_path = File.expand_path(file_arg, Dir.pwd)
+      return unless file_arg
 
+      file_path = File.expand_path(file_arg, Dir.pwd)
       appimg_path = File.join(Dir.home, ".local", "appimage")
+      dest_path = File.join(appimg_path, File.basename(file_path))
 
       begin
         unless File.directory?(appimg_path)
-          puts "\e[33m- - - - [SUBP] - alm: No appimage directory founded\e[0m"
+          puts "\e[33m- - - - [SUBP] - alm: No appimage directory found\e[0m"
           puts "\e[33m- - - - [SUBP] - alm: Creating appimage directory on #{appimg_path}...\e[0m"
           FileUtils.mkdir_p(appimg_path)
-          puts "\e[33m- - - - [SUBP] - alm: appimage directory created on #{appimg_path} successfully\e[0m"
-        else
-          puts "[P] - alm: Adding #{file_arg} to #{file_path}..."
+          puts "\e[33m- - - - [SUBP] - alm: Appimage directory created successfully\e[0m"
+        end
 
-          unless File.executable?(file_path)
-            puts "\e[33m- - - - [SUBP] - alm: Securing Executable Permissions to #{file_arg}..."
-            if system("chmod +x '#{file_path}'")
-              puts "\e[33m- - - - [SUBP] - alm: Executable rights granted to the #{file_arg}Executable rights granted to #{file_arg}"
-            else
-              puts "[ERROR | RUNTIME_ERROR] - alm: Failed to grant executable rights to #{file_arg}"
-            end
-          end
+        puts ">>> - alm: Adding #{file_arg} to #{dest_path}..."
 
-          if system("mv '#{file_path}' #{appimg_path}")
-            puts "[P+] - alm: #{file_arg} successfully added to destination"
+        unless File.executable?(file_path)
+          puts "\e[33m- - - - [SUBP] - alm: Securing executable permissions for #{file_arg}...\e[0m"
+          if system("chmod +x '#{file_path}'")
+            puts "\e[32m- - - - [SUBP] - alm: Executable rights granted to #{file_arg}\e[0m"
           else
-            puts "[ERROR | RUNTIME_ERROR] - alm: Failed to add #{file_arg} to destination"
+            puts "\e[31m[ERROR | RUNTIME_ERROR] - alm: Failed to grant executable rights to #{file_arg}\e[0m"
+            return
           end
         end
-      rescue Errno::EACCES
-        puts "[ERROR | Errno::EACCES] - alm: Permission denied"
-      rescue => e
-        puts "[ERROR | Unexpected error]: #{e.class} - #{e.message}"
-      end
 
+        if system("mv '#{file_path}' '#{appimg_path}'")
+          puts "\e[32m>>> - alm: #{file_arg} successfully added to #{appimg_path}\e[0m"
+        else
+          puts "\e[31m[ERROR | RUNTIME_ERROR] - alm: Failed to move #{file_arg} to #{appimg_path}\e[0m"
+        end
+
+      rescue Errno::EACCES
+        puts "\e[31m[ERROR | PERMISSION] - alm: Permission denied while accessing #{appimg_path}\e[0m"
+      rescue => e
+        puts "\e[31m[ERROR | UNEXPECTED] - alm: #{e.class} - #{e.message}\e[0m"
+      end
+    end
+  end
+
+  class REMOVE
+    def self.start(args)
+      file_arg = args.first
+      return unless file_arg
+
+      appimg_path = File.join(Dir.home, ".local", "appimage")
+      file_path = File.join(appimg_path, File.basename(file_arg))
+
+      begin
+        if File.exist?(file_path)
+          File.delete(file_path)
+          puts "\e[32m>>> - alm: #{File.basename(file_arg)} successfully removed from #{appimg_path}\e[0m"
+        else
+          puts "\e[31m[ERROR] - alm: File #{File.basename(file_arg)} not found in #{appimg_path}\e[0m"
+        end
+      rescue Errno::EACCES
+        puts "\e[31m[ERROR | PERMISSION] - alm: Permission denied while trying to delete #{file_path}\e[0m"
+      rescue => e
+        puts "\e[31m[ERROR | UNEXPECTED] - alm: #{e.class} - #{e.message}\e[0m"
+      end
     end
   end
 end
